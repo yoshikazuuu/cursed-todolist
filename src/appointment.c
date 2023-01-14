06107798044089873d4cfc3608871103104a31b8
rxyhn <rxyhn.dev@gmail.com>
2023-01-15 02:42:25 +0700

    feat: calendar and appointment updates

--- src/appointment.c
+++ src/appointment.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define __USE_XOPEN
 #include <time.h>
 
 struct Appointment {
@@ -11,34 +9,21 @@ struct Appointment {
   char *description;
 };
 
-int validateTime(struct tm *time) {
-  if (time->tm_year < 1900 || time->tm_year > 2100) {
-    printf("Invalid year\n");
-    return 0;
+// create appointments.txt if don't exists
+void createAppointmentsFile() {
+  FILE *appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    appointments_file = fopen("appointments.txt", "w");
+    fclose(appointments_file);
+  } else {
+    fclose(appointments_file);
   }
-  if (time->tm_mon < 1 || time->tm_mon > 12) {
-    printf("Invalid month\n");
-    return 0;
-  }
-  if (time->tm_mday < 1 || time->tm_mday > 31) {
-    printf("Invalid day\n");
-    return 0;
-  }
-  if (time->tm_hour < 0 || time->tm_hour > 23) {
-    printf("Invalid hour\n");
-    return 0;
-  }
-  if (time->tm_min < 0 || time->tm_min > 59) {
-    printf("Invalid minute\n");
-    return 0;
-  }
-  return 1;
 }
 
 void createAppointment(struct Appointment *appointment) {
-  FILE *fp = fopen("appointments.txt", "a");
-  if (fp == NULL) {
-    printf("Error opening file\n");
+  FILE *appointments_file = fopen("appointments.txt", "a");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
     return;
   }
 
@@ -46,136 +31,370 @@ void createAppointment(struct Appointment *appointment) {
   scanf("%d-%d-%d %d:%d", &appointment->startTime.tm_year,
         &appointment->startTime.tm_mon, &appointment->startTime.tm_mday,
         &appointment->startTime.tm_hour, &appointment->startTime.tm_min);
-  getchar();
 
-  if (!validateTime(&appointment->startTime)) {
-    return;
-  }
+  getchar();
 
   printf("Enter end time (YYYY-MM-DD HH:MM): ");
   scanf("%d-%d-%d %d:%d", &appointment->endTime.tm_year,
         &appointment->endTime.tm_mon, &appointment->endTime.tm_mday,
         &appointment->endTime.tm_hour, &appointment->endTime.tm_min);
+
   getchar();
-
-  if (!validateTime(&appointment->endTime)) {
-    return;
-  }
-
-  if (mktime(&appointment->startTime) > mktime(&appointment->endTime)) {
-    printf("End time must be after start time\n");
-    return;
-  }
 
   char *description = malloc(100);
   printf("Enter description: ");
-  fgets(description, 100, stdin);
-  getchar();
+  scanf("%[^\n]s", description);
   appointment->description = description;
 
-  fprintf(fp, "%d-%d-%d %d:%d %d-%d-%d %d:%d %s\n",
+  fprintf(appointments_file, "%d-%d-%d %d:%d~%d-%d-%d %d:%d~%s\n",
           appointment->startTime.tm_year, appointment->startTime.tm_mon,
           appointment->startTime.tm_mday, appointment->startTime.tm_hour,
           appointment->startTime.tm_min, appointment->endTime.tm_year,
           appointment->endTime.tm_mon, appointment->endTime.tm_mday,
           appointment->endTime.tm_hour, appointment->endTime.tm_min,
           appointment->description);
-
+  fclose(appointments_file);
   free(description);
-
-  fclose(fp);
 }
 
+// list all appointments present on appointments.txt, it should parse the file
+// that has the following format: YYYY-MM-DD HH:MM~YYYY-MM-DD HH:MM~Description
+// display format for dates should be like this:
+// Sunday, 15 January 2023 - 23:12
 void listAppointments() {
-  FILE *fp = fopen("appointments.txt", "r");
-  if (fp == NULL) {
-    printf("Error opening file\n");
+  FILE *appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
     return;
   }
 
   char *line = malloc(100);
-  while (fgets(line, 100, fp) != NULL) {
-    printf("%s", line);
+  while (fgets(line, 100, appointments_file) != NULL) {
+    char *start_time = strtok(line, "~");
+    char *end_time = strtok(NULL, "~");
+    char *description = strtok(NULL, "~");
+
+    struct tm start_time_tm;
+    struct tm end_time_tm;
+
+    strptime(start_time, "%Y-%m-%d %H:%M", &start_time_tm);
+    strptime(end_time, "%Y-%m-%d %H:%M", &end_time_tm);
+
+    char start_time_str[100];
+    char end_time_str[100];
+
+    strftime(start_time_str, 100, "%A, %d %B %Y - %H:%M", &start_time_tm);
+    strftime(end_time_str, 100, "%A, %d %B %Y - %H:%M", &end_time_tm);
+
+    printf("start date: %s\n", start_time_str);
+    printf("end date: %s\n", end_time_str);
+    printf("description: %s\n", description);
   }
-
+  fclose(appointments_file);
   free(line);
-  fclose(fp);
-
-  return;
 }
 
-void deleteAppointment() {
-  FILE *fp = fopen("appointments.txt", "r");
-  if (fp == NULL) {
-    printf("Error opening file\n");
+// do the same of list appointments but only for the month of the given date
+void listAppointmentsByMonth(struct tm date) {
+  FILE *appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
     return;
   }
 
-  int lineCount = 0;
   char *line = malloc(100);
-  while (fgets(line, 100, fp) != NULL) {
-    lineCount++;
-  }
-  free(line);
-  fclose(fp);
+  while (fgets(line, 100, appointments_file) != NULL) {
+    char *start_time = strtok(line, "~");
+    char *end_time = strtok(NULL, "~");
+    char *description = strtok(NULL, "~");
 
-  char **lines = malloc(lineCount * sizeof(char *));
-  fp = fopen("appointments.txt", "r");
-  for (int i = 0; i < lineCount; i++) {
-    lines[i] = malloc(100);
-    fgets(lines[i], 100, fp);
-  }
-  fclose(fp);
+    struct tm start_time_tm;
+    struct tm end_time_tm;
 
-  for (int i = 0; i < lineCount; i++) {
-    printf("%d. %s", i + 1, lines[i]);
-  }
+    strptime(start_time, "%Y-%m-%d %H:%M", &start_time_tm);
+    strptime(end_time, "%Y-%m-%d %H:%M", &end_time_tm);
 
-  int lineNumber;
-  printf("\nEnter line number to delete: ");
-  scanf("%d", &lineNumber);
-  getchar();
+    if (start_time_tm.tm_mon == date.tm_mon) {
+      char start_time_str[100];
+      char end_time_str[100];
 
-  free(lines[lineNumber - 1]);
-  for (int i = lineNumber - 1; i < lineCount - 1; i++) {
-    lines[i] = lines[i + 1];
-  }
+      strftime(start_time_str, 100, "%A, %d %B %Y - %H:%M", &start_time_tm);
+      strftime(end_time_str, 100, "%A, %d %B %Y - %H:%M", &end_time_tm);
 
-  fp = fopen("appointments.txt", "w");
-  for (int i = 0; i < lineCount - 1; i++) {
-    fprintf(fp, "%s", lines[i]);
-  }
-  fclose(fp);
-
-  free(lines);
-}
-
-// ! FOR TESTING PURPOSES ONLY
-int main() {
-  struct Appointment appointment;
-  int choice;
-  while (1) {
-    printf("1. Create appointment\n");
-    printf("2. List appointments\n");
-    printf("3. Delete appointment\n");
-    printf("4. Exit\n");
-    printf("Enter choice: ");
-    scanf("%d", &choice);
-
-    switch (choice) {
-    case 1:
-      createAppointment(&appointment);
-      break;
-    case 2:
-      listAppointments();
-      break;
-    case 3:
-      deleteAppointment();
-      break;
-    case 4:
-      return 0;
-    default:
-      printf("Invalid choice\n");
+      printf("start date: %s\n", start_time_str);
+      printf("end date: %s\n", end_time_str);
+      printf("description: %s\n", description);
     }
   }
+  fclose(appointments_file);
+  free(line);
+}
+
+// remove an appointment from the file by asking for the index of the line to
+// remove from
+void removeAppointment() {
+  FILE *appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
+    return;
+  }
+
+  char *line = malloc(100);
+  int line_number = 0;
+  while (fgets(line, 100, appointments_file) != NULL) {
+    char *start_time = strtok(line, "~");
+    char *end_time = strtok(NULL, "~");
+    char *description = strtok(NULL, "~");
+
+    struct tm start_time_tm;
+    struct tm end_time_tm;
+
+    strptime(start_time, "%Y-%m-%d %H:%M", &start_time_tm);
+    strptime(end_time, "%Y-%m-%d %H:%M", &end_time_tm);
+
+    char start_time_str[100];
+    char end_time_str[100];
+
+    strftime(start_time_str, 100, "%A, %d %B %Y - %H:%M", &start_time_tm);
+    strftime(end_time_str, 100, "%A, %d %B %Y - %H:%M", &end_time_tm);
+
+    printf("%d. start date: %s\n", line_number, start_time_str);
+    printf("end date: %s\n", end_time_str);
+    printf("description: %s\n", description);
+    line_number++;
+  }
+  fclose(appointments_file);
+  free(line);
+
+  printf("Enter the line number to remove: ");
+  int line_to_remove;
+  scanf("%d", &line_to_remove);
+
+  FILE *new_appointments_file = fopen("new_appointments.txt", "w");
+  if (new_appointments_file == NULL) {
+    printf("Error: Could not open new appointments file\n");
+    return;
+  }
+
+  appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
+    return;
+  }
+
+  line_number = 0;
+  while (fgets(line, 100, appointments_file) != NULL) {
+    if (line_number != line_to_remove) {
+      fprintf(new_appointments_file, "%s", line);
+    }
+    line_number++;
+  }
+  fclose(appointments_file);
+  fclose(new_appointments_file);
+  remove("appointments.txt");
+  rename("new_appointments.txt", "appointments.txt");
+  free(line);
+}
+
+// update an appointment by asking for the line to update from
+// it also should for the name of the field to update and the new value for it
+// remember to use appropiate api to time.h fields
+void updateAppointment() {
+  FILE *appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
+    return;
+  }
+
+  char *line = malloc(100);
+  int line_number = 0;
+  while (fgets(line, 100, appointments_file) != NULL) {
+    char *start_time = strtok(line, "~");
+    char *end_time = strtok(NULL, "~");
+    char *description = strtok(NULL, "~");
+
+    struct tm start_time_tm;
+    struct tm end_time_tm;
+
+    strptime(start_time, "%Y-%m-%d %H:%M", &start_time_tm);
+    strptime(end_time, "%Y-%m-%d %H:%M", &end_time_tm);
+
+    char start_time_str[100];
+    char end_time_str[100];
+
+    strftime(start_time_str, 100, "%A, %d %B %Y - %H:%M", &start_time_tm);
+    strftime(end_time_str, 100, "%A, %d %B %Y - %H:%M", &end_time_tm);
+
+    printf("%d. start date: %s\n", line_number, start_time_str);
+    printf("end date: %s\n", end_time_str);
+    printf("description: %s\n", description);
+    line_number++;
+  }
+  fclose(appointments_file);
+  free(line);
+
+  printf("Enter the line number to update: ");
+  int line_to_update;
+  scanf("%d", &line_to_update);
+
+  FILE *new_appointments_file = fopen("new_appointments.txt", "w");
+  if (new_appointments_file == NULL) {
+    printf("Error: Could not open new appointments file\n");
+    return;
+  }
+
+  appointments_file = fopen("appointments.txt", "r");
+  if (appointments_file == NULL) {
+    printf("Error: Could not open appointments file\n");
+    return;
+  }
+
+  line_number = 0;
+  while (fgets(line, 100, appointments_file) != NULL) {
+    if (line_number != line_to_update) {
+      fprintf(new_appointments_file, "%s", line);
+    } else {
+      char *start_time = strtok(line, "~");
+      char *end_time = strtok(NULL, "~");
+      char *description = strtok(NULL, "~");
+
+      struct tm start_time_tm;
+      struct tm end_time_tm;
+
+      strptime(start_time, "%Y-%m-%d %H:%M", &start_time_tm);
+      strptime(end_time, "%Y-%m-%d %H:%M", &end_time_tm);
+
+      char start_time_str[100];
+      char end_time_str[100];
+
+      strftime(start_time_str, 100, "%A, %d %B %Y - %H:%M", &start_time_tm);
+      strftime(end_time_str, 100, "%A, %d %B %Y - %H:%M", &end_time_tm);
+
+      printf("start date: %s\n", start_time_str);
+      printf("end date: %s\n", end_time_str);
+      printf("description: %s\n", description);
+
+      printf("Enter the field to update (start_time, end_time, description): ");
+      char field_to_update[100];
+      scanf("%s", field_to_update);
+
+      if (strcmp(field_to_update, "start_time") == 0) {
+        printf("Enter the new start time: ");
+        char new_start_time[100];
+        getchar();
+        scanf("%[^\n]s", new_start_time);
+        fprintf(new_appointments_file, "%s~%s~%s\n", new_start_time, end_time,
+                description);
+      } else if (strcmp(field_to_update, "end_time") == 0) {
+        printf("Enter the new end time: ");
+        char new_end_time[100];
+        getchar();
+        scanf("%[^\n]s", new_end_time);
+        fprintf(new_appointments_file, "%s~%s~%s\n", start_time, new_end_time,
+                description);
+      } else if (strcmp(field_to_update, "description") == 0) {
+        printf("Enter the new description: ");
+        char new_description[100];
+        getchar();
+        scanf("%[^\n]s", new_description);
+        fprintf(new_appointments_file, "%s~%s~%s\n", start_time, end_time,
+                new_description);
+      } else {
+        printf("Error: Invalid field to update\n");
+        exit(1);
+      }
+    }
+    line_number++;
+  }
+  fclose(appointments_file);
+  fclose(new_appointments_file);
+  remove("appointments.txt");
+  rename("new_appointments.txt", "appointments.txt");
+  free(line);
+}
+
+void display_calendar() {
+  time_t current_time = time(NULL);
+  struct tm *tm = localtime(&current_time);
+
+  int year = tm->tm_year + 1900;
+  int month = tm->tm_mon + 1;
+  int day = tm->tm_mday;
+
+  int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  if (month == 2 && year % 4 == 0) {
+    if (year % 100 == 0 && year % 400 != 0) {
+      days_in_month[2] = 28;
+    } else {
+      days_in_month[2] = 29;
+    }
+  }
+
+  printf("Su Mo Tu We Th Fr Sa\n");
+
+  int first_day_of_month = tm->tm_wday - tm->tm_mday % 7;
+  if (first_day_of_month < 0) {
+    first_day_of_month += 7;
+  }
+
+  for (int i = 0; i < first_day_of_month; i++) {
+    printf("   ");
+  }
+
+  for (int i = 1; i <= days_in_month[month]; i++) {
+    if (i == day) {
+      printf("\033[1;31m%2d\033[0m ", i);
+    } else {
+      printf("%2d ", i);
+    }
+
+    if ((i + first_day_of_month) % 7 == 0) {
+      printf("\n");
+    }
+  }
+
+  printf("\n\n");
+}
+
+void menu() {
+  while (1) {
+    display_calendar();
+    time_t current_time = time(NULL);
+    struct tm *tm = localtime(&current_time);
+    listAppointmentsByMonth(*tm);
+    printf("1. Add appointment\n");
+    printf("2. Update appointment\n");
+    printf("3. Remove appointment\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+    int choice;
+    scanf("%d", &choice);
+    system("clear");
+    display_calendar();
+    if (choice == 1) {
+      struct Appointment *appointment;
+      createAppointment(&appointment);
+    } else if (choice == 2) {
+      updateAppointment();
+    } else if (choice == 3) {
+      removeAppointment();
+    } else if (choice == 4) {
+      system("clear");
+      printf("Goodbye!\n");
+      exit(0);
+    } else {
+      printf("Error: Invalid choice\n");
+      exit(1);
+    }
+    sleep(1);
+    system("clear");
+  }
+}
+
+int main() {
+  system("clear");
+  createAppointmentsFile();
+  menu();
+  return 0;
 }
